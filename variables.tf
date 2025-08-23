4a45b59a6d1ccd46be6869097e2721065fd0c625
greg <greg-github@lucidsolutions.co.nz>
2025-08-23 15:10:10 +1200

    minor upgrade on the default (stable) flatcar container linux version

--- variables.tf
+++ variables.tf
@@ -163,7 +163,7 @@ variable flatcar_version {
     Linux will automatically upgrade if it is out of date.
 EOT
   type        = string
-  default     = "4230.2.1"
+  default     = "4230.2.2"
 }
 
 variable flatcar_channel {
